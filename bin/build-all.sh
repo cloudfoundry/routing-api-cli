9cbfb424ec7ad5bd872f1933404e48fa7368defa
Chris Piraino and Mark St.Godard <markstgodard@gmail.com>
2015-07-01 15:37:54 -0700

    Use -p flag on mkdir.

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -5,9 +5,7 @@ set -x
 
 OUTDIR=$(dirname $0)/../out
 
-if [ ! -d out ]; then
-  mkdir out
-fi
+mkdir -p out
 
 GOARCH=amd64 GOOS=linux go build -o rtr && mv rtr $OUTDIR/rtr-linux-amd64
 GOARCH=amd64 GOOS=darwin go build -o rtr && mv rtr $OUTDIR/rtr-darwin-amd64
