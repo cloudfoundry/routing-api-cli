a62b323f0da437969cfdc23f63ab01db64a4dba3
Geoff Franks <gfranks@vmware.com>
2023-03-23 15:52:14 +0000

    Convert to ginkgo v2

--- main_test.go
+++ main_test.go
@@ -13,7 +13,7 @@ import (
 
 	routing_api "code.cloudfoundry.org/routing-api"
 	"code.cloudfoundry.org/routing-api/models"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
